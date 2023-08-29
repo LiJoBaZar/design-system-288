1ed07a129869cd3ee2e71c788faad427392f97cc
Andres Reyes <areyesdev@gmail.com>
2023-08-29 11:03:55 -0500

    add:atoms icons

---
+++ src/icons/icons.tsx
@@ -0,0 +1,37 @@
+import arrowLeft from "./arrow-left.svg";
+import arrowRight from "./arrow-right.svg";
+import brand from "./brand.svg";
+import check from "./check.svg";
+import chevronDown from "./chevron-down.svg";
+import chevronRight from "./chevron-right.svg";
+import chevronUp from "./chevron-up.svg";
+import close from "./close.svg";
+import facebook from "./facebook.svg";
+import github from "./github.svg";
+import info from "./info.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import menu from "./menu.svg";
+import tiktok from "./tiktok.svg";
+import twitter from "./twitter.svg";
+import user from "./user.svg";
+
+export {
+  arrowLeft,
+  arrowRight,
+  brand,
+  check,
+  chevronDown,
+  chevronRight,
+  chevronUp,
+  close,
+  facebook,
+  github,
+  info,
+  instagram,
+  linkedin,
+  menu,
+  tiktok,
+  twitter,
+  user,
+};
